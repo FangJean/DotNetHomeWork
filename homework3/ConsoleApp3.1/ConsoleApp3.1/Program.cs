850a8ea6fa9c117b70c097e9d5d638a00cd89d89
FangJean <79981078+FangJean@users.noreply.github.com>
2021-03-23 22:12:38 +0800

    Add files via upload

---
+++ homework3/ConsoleApp3.1/ConsoleApp3.1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3._1
+{
+    public interface Shape
+    {
+        void Area();
+        bool IsLegal();
+    }
+    class Rectangle : Shape
+    {
+        int a;
+        int b;
+        public Rectangle(int x,int y)
+        {
+            a = x;
+            b = y;
+        }
+        public bool IsLegal()
+        {
+            return a > 0 && b > 0;
+        }
+            public void Area()
+            {
+            if (!IsLegal())
+            {
+                Console.WriteLine("illegal");
+            }
+                Console.WriteLine(a * b);
+            }
+
+        }
+    public class Triangle : Shape
+    {
+        int a;
+        int b;
+        int c;
+        public Triangle(int x,int y,int z)
+        {
+            a = x;
+            b = y;
+            c = z;
+        }
+        public bool IsLegal()
+        {
+            return a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a;
+        }
+        public void Area()
+        {
+            if (!IsLegal())
+            {
+                Console.WriteLine("illegal");
+            }
+            Console.WriteLine(0.25 * Math.Sqrt((a + b + c) * (a + b - c) * (a + c - b) * (b + c - a)));
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s = "";
+            int w = 0;
+            int q = 0;
+            s = Console.ReadLine();
+            w = int.Parse(s);
+            s = Console.ReadLine();
+            q = int.Parse(s);
+            Shape square = new Rectangle(w,q);
+            square.Area();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ homework3/ConsoleApp3.2/ConsoleApp3.2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3._2
+{
+    public interface Shape
+    {
+        void Area();
+        bool IsLegal();
+    }
+    class Rectangle : Shape
+    {
+        double a;
+        double b;
+        public Rectangle(double x, double y)
+        {
+            a = x;
+            b = y;
+        }
+        public bool IsLegal()
+        {
+            return a > 0 && b > 0;
+        }
+        public void Area()
+        {
+            if (!IsLegal())
+            {
+                Console.WriteLine("illegal");
+            }
+            Console.WriteLine(a * b);
+        }
+
+    }
+    public class Triangle : Shape
+    {
+        double a;
+        double b;
+        double c;
+        public Triangle(double x, double y, double z)
+        {
+            a = x;
+            b = y;
+            c = z;
+        }
+        public bool IsLegal()
+        {
+            return a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a;
+        }
+        public void Area()
+        {
+            if (!IsLegal())
+            {
+                Console.WriteLine("illegal");
+            }
+            Console.WriteLine(0.25 * Math.Sqrt((a + b + c) * (a + b - c) * (a + c - b) * (b + c - a)));
+        }
+    }
+    class Factory
+    {
+        public static Shape RanShape(int ran)
+        {
+            Random rd = new Random(ran);
+            int k = rd.Next(0, 2);
+            Shape shape=null;
+            Console.Write(k);
+            if (k == 0)
+            {
+                shape = new Triangle(rd.NextDouble() + rd.Next(1, 5), rd.NextDouble() + rd.Next(1, 5), rd.NextDouble() + rd.Next(1, 5));
+                Console.WriteLine("三角形");
+            }
+            else if (k == 1)
+            {
+                shape = new Rectangle(rd.NextDouble() + rd.Next(0, 5), rd.NextDouble() + rd.Next(0, 5));
+                Console.WriteLine("矩形");
+            }
+            return shape;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Shape s = Factory.RanShape(i);
+                s.Area();
+            }
+            Console.ReadKey();
+        }
+    }
+}
